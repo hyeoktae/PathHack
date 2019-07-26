df479aa06ba9355dc4a04e4cc95d02378f2b36cd
dldbdjq <dldbdjq@gmail.com>
2019-07-27 01:18:15 +0900

    RewardStoreViewcontroller

---
+++ PathFinder/Cells/MapDetail/CommunityCell.swift
@@ -0,0 +1,59 @@
+//
+//  CommunityCell.swift
+//  PathFinder
+//
+//  Created by Lee on 26/07/2019.
+//  Copyright © 2019 Fury. All rights reserved.
+//
+
+import UIKit
+
+class CommunityCell: UITableViewCell {
+
+  static let identifier = "CommunityCell"
+  
+  private let titleLabel = UILabel()
+  private let contentsLabel = UILabel()
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    
+    configure()
+    autoLayout()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setting() {
+    
+  }
+  
+  private func configure() {
+    self.selectionStyle = .none
+    
+    titleLabel.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
+    contentView.addSubview(titleLabel)
+    
+    contentsLabel.numberOfLines = 0
+    contentView.addSubview(contentsLabel)
+  }
+  
+  private struct Standard {
+    static let space: CGFloat = 10
+  }
+  
+  private func autoLayout() {
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: Standard.space).isActive = true
+    titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Standard.space).isActive = true
+    titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Standard.space).isActive = true
+    
+    contentsLabel.translatesAutoresizingMaskIntoConstraints = false
+    contentsLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: Standard.space).isActive = true
+    contentsLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Standard.space).isActive = true
+    contentsLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -Standard.space).isActive = true
+    contentsLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -Standard.space).isActive = true
+  }
+}
--- PathFinder/Controller/RewordStoreVC.swift
+++ PathFinder/Controller/RewordStoreVC.swift
@@ -9,22 +9,129 @@
 import UIKit
 
 class RewordStoreVC: UIViewController {
+  
+  private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+  
+  let itemColors = [UIColor.red, UIColor.yellow, UIColor.blue, UIColor.green]
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    print("Hi")
+    
+    navigationSet()
+    configure()
+    autoLayout()
+  }
+  
+  override func viewWillLayoutSubviews() {
+    super.viewWillLayoutSubviews()
+    
+    // width, height 설정
+    let cellWidth = view.frame.width - 20 - 15
+    let cellHeight = view.frame.height - view.safeAreaInsets.top - view.safeAreaInsets.bottom
+    
+    print("cellWidth : ", cellWidth)
+    print("cellHeight : ", cellHeight)
+    
+    // 상하, 좌우 inset value 설정
+    let insetX: CGFloat = 10
+    let insetY: CGFloat = 10
+    
+    print("width : ", collectionView.bounds.width)
+    print("height : ", collectionView.bounds.height)
+    
+    let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+    layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+    layout.minimumLineSpacing = 15
+    layout.scrollDirection = .horizontal
+    collectionView.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: insetY, right: insetX)
+    
+    collectionView.delegate = self
+    collectionView.dataSource = self
+    
+    // 스크롤 시 빠르게 감속 되도록 설정
+    collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+  }
+  
+  private func navigationSet() {
+    navigationItem.title = ""
+    navigationController?.navigationBar.prefersLargeTitles = true
+    navigationController?.navigationBar.shadowImage = UIImage()
+    navigationController?.navigationBar.barTintColor = .white
+  }
+  
+  private func configure() {
+    view.backgroundColor = .white
+    
+    collectionView.register(RewardStoreCell.self, forCellWithReuseIdentifier: RewardStoreCell.identifier)
+    view.addSubview(collectionView)
+  }
+  
+  private struct Standard {
+    static let space: CGFloat = 10
+  }
+  
+  private func autoLayout() {
+    let guide = view.safeAreaLayoutGuide
+    
+    collectionView.translatesAutoresizingMaskIntoConstraints = false
+    collectionView.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
+    collectionView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+    collectionView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+    collectionView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+  }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+extension RewordStoreVC: UICollectionViewDataSource, UICollectionViewDelegate {
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return itemColors.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RewardStoreCell.identifier, for: indexPath) as! RewardStoreCell
+    
+    cell.backgroundColor = itemColors[indexPath.row]
+    
+    return cell
+  }
+}
 
-        // Do any additional setup after loading the view.
+extension RewordStoreVC : UIScrollViewDelegate
+{
+  func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>)
+  {
+    // item의 사이즈와 item 간의 간격 사이즈를 구해서 하나의 item 크기로 설정.
+    let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+    let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+    print("cellWidthIncludingSpacing", cellWidthIncludingSpacing)
+    
+    // targetContentOff을 이용하여 x좌표가 얼마나 이동했는지 확인
+    // 이동한 x좌표 값과 item의 크기를 비교하여 몇 페이징이 될 것인지 값 설정
+    var offset = targetContentOffset.pointee
+    let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
+    var roundedIndex = round(index)
+    print("index", index)
+    print("roundedIndex", roundedIndex)
+    
+    // scrollView, targetContentOffset의 좌표 값으로 스크롤 방향을 알 수 있다.
+    // index를 반올림하여 사용하면 item의 절반 사이즈만큼 스크롤을 해야 페이징이 된다.
+    // 스크로로 방향을 체크하여 올림,내림을 사용하면 좀 더 자연스러운 페이징 효과를 낼 수 있다.
+    if scrollView.contentOffset.x > targetContentOffset.pointee.x {
+      roundedIndex = floor(index)
+    } else {
+      roundedIndex = ceil(index)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // 위 코드를 통해 페이징 될 좌표값을 targetContentOffset에 대입하면 된다.
+    offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
+    targetContentOffset.pointee = offset
+    print("offset", offset)
+  }
 }
+
