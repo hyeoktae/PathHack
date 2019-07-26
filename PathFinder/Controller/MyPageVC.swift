6b64791ccf2a37b7f9d636e93002952967732a1a
dldbdjq <dldbdjq@gmail.com>
2019-07-27 05:36:56 +0900

    ADD: RewoadVC, MyPage

--- PathFinder/Controller/MyPageVC.swift
+++ PathFinder/Controller/MyPageVC.swift
@@ -10,25 +10,138 @@ import UIKit
 
 class MyPageVC: UIViewController {
   
-  private let myPageView: MyPageView = {
-    let myPageView = MyPageView()
-    myPageView.translatesAutoresizingMaskIntoConstraints = false
-    return myPageView
-  }()
+  private let baseView = UIView()
+  private let idView = UIView()
+  private let imageView = UIImageView()
+  private let idLabel = UILabel()
+  private let lineView = UIView()
+  private let contentsButton = UIButton()
+  private let contentsline = UILabel()
+  private let reportButton = UIButton()
+  private let reportline = UILabel()
+  private let appPointLabel = UILabel()
+  private let appPointline = UILabel()
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    setupMyPageView()
+    
+    
+    navigationSet()
+    configure()
+    autoLayout()
   }
   
-  private func setupMyPageView() {
+  private func navigationSet() {
+    navigationItem.title = "My Page"
+    navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.6941176471, green: 0.8666666667, blue: 0.8509803922, alpha: 1)
+  }
+  
+  private func configure() {
+    view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+    
+    baseView.backgroundColor = .white
+    view.addSubview(baseView)
+    
+    idView.backgroundColor = .white
+    idView.layer.cornerRadius = 10
+    view.addSubview(idView)
+    
+    imageView.image = UIImage(named: "profile")
+    idView.addSubview(imageView)
+    
+    idLabel.text = "금정구 보안관"
+    idView.addSubview(idLabel)
+    
+    lineView.layer.cornerRadius = 10
+    lineView.layer.borderColor = UIColor.darkGray.cgColor
+    lineView.layer.borderWidth = 1
+    view.addSubview(lineView)
+    
+    contentsButton.setTitle("내가 쓴 글", for: .normal)
+    contentsButton.setTitleColor(.black, for: .normal)
+    contentsButton.backgroundColor = .white
+    contentsButton.layer.cornerRadius = 10
+    view.addSubview(contentsButton)
+    
+    contentsline.backgroundColor = .darkGray
+    view.addSubview(contentsline)
+    
+    reportButton.setTitle("내가 신고한 내역", for: .normal)
+    reportButton.setTitleColor(.black, for: .normal)
+    reportButton.backgroundColor = .white
+    reportButton.layer.cornerRadius = 10
+    view.addSubview(reportButton)
+    
+    reportline.backgroundColor = .darkGray
+    view.addSubview(reportline)
+    
+    appPointLabel.text = "내 포인트 : 900"
+    appPointLabel.textAlignment = .center
+    appPointLabel.backgroundColor = .white
+    appPointLabel.layer.cornerRadius = 10
+    appPointLabel.layer.masksToBounds = true
+    view.addSubview(appPointLabel)
+  }
+  
+  private struct Standard {
+    static let space: CGFloat = 10
+  }
+  
+  private func autoLayout() {
     let guide = view.safeAreaLayoutGuide
     
-    view.addSubview(myPageView)
-    myPageView.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
-    myPageView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
-    myPageView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
-    myPageView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+    baseView.translatesAutoresizingMaskIntoConstraints = false
+    baseView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 20).isActive = true
+    baseView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 20).isActive = true
+    baseView.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -20).isActive = true
+    baseView.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -20).isActive = true
+    
+    idView.translatesAutoresizingMaskIntoConstraints = false
+    idView.topAnchor.constraint(equalTo: guide.topAnchor, constant: 70).isActive = true
+    idView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 30).isActive = true
+    idView.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -30).isActive = true
+    idView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    imageView.centerYAnchor.constraint(equalTo: idView.centerYAnchor).isActive = true
+    imageView.leadingAnchor.constraint(equalTo: idView.leadingAnchor, constant: 40).isActive = true
+    
+    idLabel.translatesAutoresizingMaskIntoConstraints = false
+    idLabel.topAnchor.constraint(equalTo: idView.topAnchor).isActive = true
+    idLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 30).isActive = true
+    idLabel.bottomAnchor.constraint(equalTo: idView.bottomAnchor).isActive = true
+    
+    lineView.translatesAutoresizingMaskIntoConstraints = false
+    lineView.topAnchor.constraint(equalTo: contentsButton.topAnchor, constant: -10).isActive = true
+    lineView.leadingAnchor.constraint(equalTo: baseView.leadingAnchor, constant: 10).isActive = true
+    lineView.trailingAnchor.constraint(equalTo: baseView.trailingAnchor, constant: -10).isActive = true
+    lineView.bottomAnchor.constraint(equalTo: appPointLabel.bottomAnchor, constant: 10).isActive = true
+    
+    contentsButton.translatesAutoresizingMaskIntoConstraints = false
+    contentsButton.centerXAnchor.constraint(equalTo: guide.centerXAnchor).isActive = true
+    contentsButton.topAnchor.constraint(equalTo: idView.bottomAnchor, constant: 40).isActive = true
+    contentsButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    contentsline.translatesAutoresizingMaskIntoConstraints = false
+    contentsline.leadingAnchor.constraint(equalTo: lineView.leadingAnchor, constant:  10).isActive = true
+    contentsline.trailingAnchor.constraint(equalTo: lineView.trailingAnchor, constant: -10).isActive = true
+    contentsline.bottomAnchor.constraint(equalTo: contentsButton.bottomAnchor, constant: 10).isActive = true
+    contentsline.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    
+    reportButton.translatesAutoresizingMaskIntoConstraints = false
+    reportButton.centerXAnchor.constraint(equalTo: guide.centerXAnchor).isActive = true
+    reportButton.topAnchor.constraint(equalTo: contentsButton.bottomAnchor, constant: 20).isActive = true
+    reportButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    reportline.translatesAutoresizingMaskIntoConstraints = false
+    reportline.leadingAnchor.constraint(equalTo: lineView.leadingAnchor, constant: 10).isActive = true
+    reportline.trailingAnchor.constraint(equalTo: lineView.trailingAnchor, constant: -10).isActive = true
+    reportline.bottomAnchor.constraint(equalTo: reportButton.bottomAnchor, constant: 10).isActive = true
+    reportline.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    
+    appPointLabel.translatesAutoresizingMaskIntoConstraints = false
+    appPointLabel.centerXAnchor.constraint(equalTo: guide.centerXAnchor).isActive = true
+    appPointLabel.topAnchor.constraint(equalTo: reportButton.bottomAnchor, constant: 20).isActive = true
+    appPointLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
   }
 }
--- PathFinder/Controller/RewordStoreVC.swift
+++ PathFinder/Controller/RewordStoreVC.swift
@@ -8,116 +8,141 @@
 
 import UIKit
 
+struct Store {
+  let commpany: String
+  let detail: String
+  let appPoint: Int
+}
+
 class RewordStoreVC: UIViewController {
   
   private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+  private var cellWidth: CGFloat = 0
+  private var cellHeight: CGFloat = 0
+  private let pageControl = UIPageControl()
   
-  let itemColors = [UIColor.red, UIColor.yellow, UIColor.blue, UIColor.green]
+  var data = [
+    Store(commpany: "배스킨라빈스", detail: "닐라닐라 바닐라", appPoint: 100),
+    Store(commpany: "배스킨라빈스", detail: "닐라닐라 바닐라", appPoint: 100),
+    Store(commpany: "배스킨라빈스", detail: "닐라닐라 바닐라", appPoint: 100),
+    Store(commpany: "배스킨라빈스", detail: "닐라닐라 바닐라", appPoint: 100),
+    Store(commpany: "배스킨라빈스", detail: "닐라닐라 바닐라", appPoint: 100),
+  ]
+  
+  private var groupCount = 0 {
+    willSet {
+      pageControl.currentPage = newValue
+    }
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    
-    print("Hi")
     
     navigationSet()
     configure()
     autoLayout()
   }
   
+  private var layoutState = true
   override func viewWillLayoutSubviews() {
     super.viewWillLayoutSubviews()
-    
-    // width, height 설정
-    let cellWidth = view.frame.width - 20 - 15
-    let cellHeight = view.frame.height - view.safeAreaInsets.top - view.safeAreaInsets.bottom
-    
-    print("cellWidth : ", cellWidth)
-    print("cellHeight : ", cellHeight)
-    
-    // 상하, 좌우 inset value 설정
-    let insetX: CGFloat = 10
-    let insetY: CGFloat = 10
-    
-    print("width : ", collectionView.bounds.width)
-    print("height : ", collectionView.bounds.height)
-    
-    let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-    layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
-    layout.minimumLineSpacing = 15
-    layout.scrollDirection = .horizontal
-    collectionView.contentInset = UIEdgeInsets(top: insetY, left: insetX, bottom: insetY, right: insetX)
-    
-    collectionView.delegate = self
-    collectionView.dataSource = self
-    
-    // 스크롤 시 빠르게 감속 되도록 설정
-    collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+    if layoutState {
+      let collectionViewWidth = view.frame.width - (view.safeAreaInsets.right + view.safeAreaInsets.left)
+      let cellectionViewHeight = view.frame.height - (view.safeAreaInsets.top + view.safeAreaInsets.bottom + 50)
+      
+      cellWidth = collectionViewWidth - (Standard.space * 2)
+      cellHeight = cellectionViewHeight - (Standard.space * 2)
+      
+      let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+      layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+      layout.minimumLineSpacing = Standard.minimumLineSpacing
+      layout.scrollDirection = .horizontal
+      collectionView.contentInset = UIEdgeInsets(top: Standard.space, left: Standard.space, bottom: Standard.space, right: Standard.space)
+    }
+    layoutState = false
   }
   
   private func navigationSet() {
-    navigationItem.title = ""
-    navigationController?.navigationBar.prefersLargeTitles = true
-    navigationController?.navigationBar.shadowImage = UIImage()
-    navigationController?.navigationBar.barTintColor = .white
+    navigationItem.title = "Store"
   }
   
+  
   private func configure() {
-    view.backgroundColor = .white
+    view.backgroundColor = #colorLiteral(red: 0.6941176471, green: 0.8666666667, blue: 0.8509803922, alpha: 1)
     
-    collectionView.register(RewardStoreCell.self, forCellWithReuseIdentifier: RewardStoreCell.identifier)
+    collectionView.showsHorizontalScrollIndicator = false
+    collectionView.backgroundColor = .clear
+    collectionView.dataSource = self
+    collectionView.delegate = self
+    collectionView.decelerationRate = UIScrollView.DecelerationRate.fast
+    collectionView.register(RewoadCell.self, forCellWithReuseIdentifier: RewoadCell.identifier)
     view.addSubview(collectionView)
+    
+    pageControl.currentPage = 0
+    pageControl.currentPageIndicatorTintColor = .darkGray
+    pageControl.pageIndicatorTintColor = UIColor.darkGray.withAlphaComponent(0.2)
+    pageControl.numberOfPages = data.count
+    view.addSubview(pageControl)
   }
   
   private struct Standard {
-    static let space: CGFloat = 10
+    static let space: CGFloat = 40
+    static let minimumLineSpacing: CGFloat = 20
+    
+    static let sliderHeight: CGFloat = 60
   }
   
   private func autoLayout() {
-    let guide = view.safeAreaLayoutGuide
-    
     collectionView.translatesAutoresizingMaskIntoConstraints = false
-    collectionView.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
-    collectionView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
-    collectionView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
-    collectionView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+    collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+    collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    
+    pageControl.translatesAutoresizingMaskIntoConstraints = false
+    pageControl.topAnchor.constraint(equalTo: collectionView.bottomAnchor).isActive = true
+    pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    pageControl.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
+    pageControl.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    pageControl.heightAnchor.constraint(equalToConstant: 30).isActive = true
+  }
+  
+  func alertAction(tilte: String?, message: String?) {
+    let alert = UIAlertController(title: tilte, message: message, preferredStyle: .alert)
+    let cancel = UIAlertAction(title: "cancel", style: .cancel) { (action) in
+      
+    }
+    alert.addAction(cancel)
+    present(alert, animated: true)
   }
 }
 
-extension RewordStoreVC: UICollectionViewDataSource, UICollectionViewDelegate {
-  func numberOfSections(in collectionView: UICollectionView) -> Int {
-    return 1
-  }
-  
+
+extension RewordStoreVC: UICollectionViewDataSource {
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return itemColors.count
+    return data.count
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RewardStoreCell.identifier, for: indexPath) as! RewardStoreCell
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RewoadCell.identifier, for: indexPath) as! RewoadCell
     
-    cell.backgroundColor = itemColors[indexPath.row]
+    cell.setting(data: data[indexPath.row])
     
     return cell
   }
 }
 
-extension RewordStoreVC : UIScrollViewDelegate
-{
-  func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>)
-  {
+extension RewordStoreVC: UICollectionViewDelegate {
+  func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
     // item의 사이즈와 item 간의 간격 사이즈를 구해서 하나의 item 크기로 설정.
     let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
     let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
-    print("cellWidthIncludingSpacing", cellWidthIncludingSpacing)
     
     // targetContentOff을 이용하여 x좌표가 얼마나 이동했는지 확인
     // 이동한 x좌표 값과 item의 크기를 비교하여 몇 페이징이 될 것인지 값 설정
     var offset = targetContentOffset.pointee
     let index = (offset.x + scrollView.contentInset.left) / cellWidthIncludingSpacing
     var roundedIndex = round(index)
-    print("index", index)
-    print("roundedIndex", roundedIndex)
+    
     
     // scrollView, targetContentOffset의 좌표 값으로 스크롤 방향을 알 수 있다.
     // index를 반올림하여 사용하면 item의 절반 사이즈만큼 스크롤을 해야 페이징이 된다.
@@ -127,11 +152,13 @@ extension RewordStoreVC : UIScrollViewDelegate
     } else {
       roundedIndex = ceil(index)
     }
-    
+    groupCount = Int(roundedIndex)
     // 위 코드를 통해 페이징 될 좌표값을 targetContentOffset에 대입하면 된다.
     offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: -scrollView.contentInset.top)
     targetContentOffset.pointee = offset
-    print("offset", offset)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    alertAction(tilte: "포인트 부족", message: "더 많은 신고 부탁드립니다.")
   }
 }
-
